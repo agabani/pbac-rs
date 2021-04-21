5d046afcf031870a05c17a2e3be53226532dc327
agabani <agabani@users.noreply.github.com>
2021-04-21 23:25:49 +0100

    feat: default evaluator allow support

--- pbac/src/lib.rs
+++ pbac/src/lib.rs
@@ -21,8 +21,28 @@ pub struct Policy {
     pub resources: Vec<Resource>,
 }
 
-pub fn evaluate() -> (Effect, Option<Policy>) {
-    (Effect::Deny, None)
+pub fn evaluate<'a>(
+    policies: &'a [Policy],
+    action: &Action,
+    resources: &[Resource],
+) -> (Effect, Vec<&'a Policy>) {
+    let allowed = policies
+        .iter()
+        .filter(|policy| {
+            policy.effect == Effect::Allow
+                && policy.actions.contains(action)
+                && policy
+                    .resources
+                    .iter()
+                    .any(|resource| resources.contains(resource))
+        })
+        .collect::<Vec<_>>();
+
+    if !allowed.is_empty() {
+        return (Effect::Allow, allowed);
+    }
+
+    (Effect::Deny, vec![])
 }
 
 #[cfg(test)]
@@ -31,9 +51,64 @@ mod tests {
 
     #[test]
     fn it_denies_by_default() {
-        let (effect, policy) = evaluate();
+        let polices = vec![];
+        let action = Action("noop".to_string());
+        let resources = vec![];
 
-        assert_eq!(Effect::Deny, effect);
-        assert_eq!(None, policy);
+        let (effect, policy) = evaluate(&polices, &action, &resources);
+
+        assert_eq!(effect, Effect::Deny);
+        assert_eq!(policy, Vec::<&Policy>::new());
+    }
+
+    #[test]
+    fn it_allows_when_policy_match() {
+        let policies = vec![Policy {
+            actions: vec![Action("account:GetAccount".to_string())],
+            effect: Effect::Allow,
+            principals: vec![Principal("user:JohnSmith".to_string())],
+            resources: vec![Resource("account:JohnSmith".to_string())],
+        }];
+        let action = Action("account:GetAccount".to_string());
+        let resources = vec![Resource("account:JohnSmith".to_string())];
+
+        let (effect, policy) = evaluate(&policies, &action, &resources);
+
+        assert_eq!(effect, Effect::Allow);
+        assert_eq!(policy, &[policy[0]]);
+    }
+
+    #[test]
+    fn it_denies_when_action_does_not_match() {
+        let policies = vec![Policy {
+            actions: vec![Action("account:GetAccount".to_string())],
+            effect: Effect::Allow,
+            principals: vec![Principal("user:JohnSmith".to_string())],
+            resources: vec![Resource("account:JohnSmith".to_string())],
+        }];
+        let action = Action("account:UpdateAccount".to_string());
+        let resources = vec![Resource("account:JohnSmith".to_string())];
+
+        let (effect, policy) = evaluate(&policies, &action, &resources);
+
+        assert_eq!(effect, Effect::Deny);
+        assert_eq!(policy, Vec::<&Policy>::new());
+    }
+
+    #[test]
+    fn it_denies_when_resource_does_not_match() {
+        let policies = vec![Policy {
+            actions: vec![Action("account:GetAccount".to_string())],
+            effect: Effect::Allow,
+            principals: vec![Principal("user:JohnSmith".to_string())],
+            resources: vec![Resource("account:JohnSmith".to_string())],
+        }];
+        let action = Action("account:GetAccount".to_string());
+        let resources = vec![Resource("account:JaneSmith".to_string())];
+
+        let (effect, policy) = evaluate(&policies, &action, &resources);
+
+        assert_eq!(effect, Effect::Deny);
+        assert_eq!(policy, Vec::<&Policy>::new());
     }
 }
