189463b3d574b1610d4c1a2b0e157896609cdcfa
agabani <agabani@users.noreply.github.com>
2021-04-21 22:52:09 +0100

    feat: default evaluator

--- pbac/src/lib.rs
+++ pbac/src/lib.rs
@@ -1,7 +1,39 @@
+#[derive(Debug, PartialEq)]
+pub struct Action(String);
+
+#[derive(Debug, PartialEq)]
+pub enum Effect {
+    Allow,
+    Deny,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Principal(String);
+
+#[derive(Debug, PartialEq)]
+pub struct Resource(String);
+
+#[derive(Debug, PartialEq)]
+pub struct Policy {
+    pub actions: Vec<Action>,
+    pub effect: Effect,
+    pub principals: Vec<Principal>,
+    pub resources: Vec<Resource>,
+}
+
+pub fn evaluate() -> (Effect, Option<Policy>) {
+    (Effect::Deny, None)
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn it_denies_by_default() {
+        let (effect, policy) = evaluate();
+
+        assert_eq!(Effect::Deny, effect);
+        assert_eq!(None, policy);
     }
 }
